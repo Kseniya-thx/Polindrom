ec1e2f41471041370c8c789d20f702b55a393012
a_marchenkov <andrej.marchenkov@gmail.com>
2022-02-15 15:58:33 +0300

    Initial commit

---
+++ core/Application.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace core
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            string str = "Bop PoB klop polk";
+            List<string> listTest = str.Split(' ').ToList();
+
+            // List of processed words to avoid repeated processing
+            List<string> alreadyProcessed = new List<string>();
+
+            foreach (string word1 in listTest)
+            {
+                foreach (string word2 in listTest)
+                {
+                    if (word1 == word2)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
---
+++ core/PolindromChecker.cs
@@ -0,0 +1,13 @@
+namespace core
+{
+    public class PolindromChecker
+    {
+        public static bool Check(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[s.Length - i - 1])
+                    return false;
+            return true;
+        }
+    }
+}
